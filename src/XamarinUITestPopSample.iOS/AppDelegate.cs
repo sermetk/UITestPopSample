c939fd91b7f4f3dd40f3d29b441387528dfa67a8
Sermet <sermetkose@gmail.com>
2020-12-07 19:29:37 +0300

    update solution folders.

---
+++ src/XamarinUITestPopSample.iOS/AppDelegate.cs
@@ -0,0 +1,19 @@
+﻿using Foundation;
+using UIKit;
+using Xamarin.Forms;
+
+namespace XamarinUITestPopSample.iOS
+{
+    [Register("AppDelegate")]
+    public partial class AppDelegate : Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            Xamarin.Calabash.Start();
+            Forms.SetFlags("CarouselView_Experimental");
+            Forms.Init();
+            LoadApplication(new App());
+            return base.FinishedLaunching(app, options);
+        }
+    }
+}
